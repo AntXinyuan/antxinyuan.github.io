a3139ba99dec60b785f769eb9f927837c2bd4bc4
liuxinyuan <ant_xinyuan@163.com>
2024-06-13 18:06:18 +0800

    Add some statistic of google scholar citation

---
+++ scripts/scholar_crawler.py
@@ -0,0 +1,14 @@
+from scholarly import scholarly
+import json
+from datetime import datetime
+import os
+
+author: dict = scholarly.search_author_id('eXwizz8AAAAJ')
+scholarly.fill(author, sections=['publications'])
+name = author['name']
+author['updated'] = str(datetime.now())
+author['publications'] = {v['author_pub_id']:v for v in author['publications']}
+print(json.dumps(author, indent=2))
+os.makedirs('results', exist_ok=True)
+with open(f'citation.json', 'w') as outfile:
+    json.dump(author, outfile, ensure_ascii=False)
