3f72e6b608a1822425b05ed3cc7e837d03b4075a
liuxinyuan <ant_xinyuan@163.com>
2025-05-26 15:10:31 +0800

    FIx some bugs

--- scripts/scholar_crawler.py
+++ scripts/scholar_crawler.py
@@ -3,12 +3,15 @@ import json
 from datetime import datetime
 import os
 
-author: dict = scholarly.search_author_id('eXwizz8AAAAJ')
-scholarly.fill(author, sections=['publications'])
-name = author['name']
-author['updated'] = str(datetime.now())
-author['publications'] = {v['author_pub_id']:v for v in author['publications']}
-print(json.dumps(author, indent=2))
-os.makedirs('results', exist_ok=True)
-with open(f'scholar.json', 'w') as outfile:
-    json.dump(author, outfile, ensure_ascii=False)
+try:
+    author: dict = scholarly.search_author_id('eXwizz8AAAAJ')
+    scholarly.fill(author, sections=['publications'])
+    name = author['name']
+    author['updated'] = str(datetime.now())
+    author['publications'] = {v['author_pub_id']:v for v in author['publications']}
+    print(json.dumps(author, indent=2))
+    os.makedirs('results', exist_ok=True)
+    with open(f'scholar.json', 'w') as outfile:
+        json.dump(author, outfile, ensure_ascii=False)
+except Exception as e:
+    print(e)
